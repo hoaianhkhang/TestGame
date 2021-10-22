9f152b7a73d601d7f332e6ecc3407888b6f69415
hoaianhkhang <hoaianhkhang@users.noreply.github.com>
2021-10-23 00:11:16 +0700

    Add File

---
+++ Assets/Germs.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Germs : MonoBehaviour
+{
+    Vector2 firstPosTouch;
+    Vector2 finaltPosTouch;
+    public Vector2 temp;
+
+    public bool checkMatched = false;
+
+    public int  moveX;
+    public int  moveY;
+
+    public int lastColumn;
+    public int lastRow;
+
+    public int germColumn;
+    public int germRow;
+    public GameObject otherGerm;
+    public boardCreateScript Board;
+
+    public float swipeAngle = 0;
+    public float swipeResist = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Board = FindObjectOfType<boardCreateScript>();
+        moveX = (int)transform.position.x;
+        moveY = (int)transform.position.y;
+        germRow = moveY;
+        germColumn = moveX;
+        lastRow = germRow;
+        lastColumn = germColumn;
+    }
+
+    private void OnMouseDown()
+    {
+        if(Board.Score <= Board.maxScore && Board.Pause == false)
+        firstPosTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+  
+    }
+
+    private void OnMouseUp()
+    {
+        if (Board.Score <= Board.maxScore && Board.Pause == false)
+        {
+            finaltPosTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            angleCal();
+        }
+    }
+
+    public IEnumerator checkMove()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        if (otherGerm != null) { 
+            if (!checkMatched && !otherGerm.GetComponent<Germs>().checkMatched)
+            {
+                otherGerm.GetComponent<Germs>().germRow = germRow;
+                otherGerm.GetComponent<Germs>().germColumn = germColumn;
+                germRow = lastRow;
+                germColumn = lastColumn;
+            }
+            else
+            {
+                Board.destroyMatchGerm();
+            }
+            otherGerm = null;
+    }
+    }
+
+    private void angleCal()
+    {
+        if (Mathf.Abs(finaltPosTouch.y - firstPosTouch.y) > swipeResist || Mathf.Abs(finaltPosTouch.x - firstPosTouch.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finaltPosTouch.y - firstPosTouch.y, finaltPosTouch.x - firstPosTouch.x) * 180 / Mathf.PI;
+
+            moveGerms();
+        }
+    }
+    void moveGerms()
+    {
+          if ((swipeAngle > 135 || swipeAngle <= -135) && germColumn > 0)
+        {
+            otherGerm = Board.listOfGerms[germColumn - 1, germRow];
+            otherGerm.GetComponent<Germs>().germColumn += 1;
+            germColumn -= 1;
+        }
+
+        else if (swipeAngle > 45 && swipeAngle <= 135 && germRow < Board.height - 1)
+        {
+            otherGerm = Board.listOfGerms[germColumn, germRow + 1];
+            otherGerm.GetComponent<Germs>().germRow -= 1;
+            germRow += 1;
+        }
+
+       
+
+        else if (swipeAngle < -45 && swipeAngle >= -135 && germRow > 0)
+        {
+            otherGerm = Board.listOfGerms[germColumn, germRow - 1];
+            otherGerm.GetComponent<Germs>().germRow += 1;
+            germRow -= 1;
+        }
+        else if(swipeAngle > -45 && swipeAngle <= 45 && germColumn < Board.width - 1)
+        {
+            otherGerm = Board.listOfGerms[germColumn + 1, germRow];
+            otherGerm.GetComponent<Germs>().germColumn -= 1;
+            germColumn += 1;
+        }
+        StartCoroutine(checkMove());
+
+    }
+
+    void findMatch()
+    {
+        if(germColumn > 0 && germColumn < Board.width -1 )
+        {
+            GameObject left = Board.listOfGerms[germColumn - 1, germRow];
+            GameObject right = Board.listOfGerms[germColumn + 1, germRow];
+            if (left != null && right != null && left != this.gameObject && right != this.gameObject)
+            {
+                if (left.tag == this.gameObject.tag && right.tag == this.gameObject.tag)
+                {
+                    right.GetComponent<Germs>().checkMatched = true;
+                    left.GetComponent<Germs>().checkMatched = true;
+                    checkMatched = true;
+                }
+            }
+        }
+        if (germRow > 0 && germRow < Board.height - 1 )
+        {
+            GameObject up = Board.listOfGerms[germColumn, germRow + 1];
+            GameObject down = Board.listOfGerms[germColumn, germRow - 1];
+            if (up != null && down != null && up != this.gameObject && down != this.gameObject)
+            {
+                if (up.tag == this.gameObject.tag && down.tag == this.gameObject.tag)
+                {
+                    up.GetComponent<Germs>().checkMatched = true;
+                    down.GetComponent<Germs>().checkMatched = true;
+                    checkMatched = true;
+                }
+            }
+        }
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        findMatch();
+        if(checkMatched)
+        {
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            sprite.color = new Color(0, 0, 0, 1);
+        }
+
+
+        moveX = germColumn;
+        moveY = germRow;
+        if(Mathf.Abs(moveX - transform.position.x) > 0.1)
+        {
+            temp = new Vector2(moveX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, temp, 0.6f);
+            if(Board.listOfGerms[germColumn,germRow] != this.gameObject)
+            {
+                Board.listOfGerms[germColumn, germRow] = this.gameObject;
+            }
+        }
+        else
+        {
+            temp = new Vector2(moveX, transform.position.y);
+            transform.position = temp;
+           
+        }
+        if (Mathf.Abs(moveY - transform.position.y) > 0.1)
+        {
+            temp = new Vector2(transform.position.x, moveY);
+            transform.position = Vector2.Lerp(transform.position, temp, .4f);
+            if (Board.listOfGerms[germColumn, germRow] != this.gameObject)
+            {
+                Board.listOfGerms[germColumn, germRow] = this.gameObject;
+            }
+        }
+        else
+        {
+            temp = new Vector2(transform.position.x, moveY);
+            transform.position = temp;
+      
+        }
+    }
+}
---
+++ Assets/boardCreateScript.cs
@@ -0,0 +1,268 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class boardCreateScript : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public List<GameObject> germ;
+
+    public GameObject[,] listOfGerms;
+
+    public int maxScore;
+    public int Score;
+    public bool Pause;
+
+    public GameObject PauseButton;
+    public GameObject RetryButton;
+    public GameObject dimImage;
+
+    public UnityEngine.UI.Text scoreText;
+
+    public void pauseGame()
+    {
+        if(Pause == false)
+        {
+            dimImage.SetActive(true);
+            RetryButton.SetActive(true);
+            Pause = true;
+        }
+        
+        else
+        {
+            dimImage.SetActive(false);
+            RetryButton.SetActive(false);
+            Pause = false;
+        }
+            
+    }
+
+    public void retryGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+
+    public void createBoard()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                int randomNumb;
+                Vector2 temp = new Vector2(i, j);
+                randomNumb = chooseRandomGerm();
+                
+                int iteration = 0;
+                while(matchesAt(i,j,germ[randomNumb]) && iteration < 100)
+                {
+                    randomNumb = chooseRandomGerm();
+                    iteration++;
+                }
+                GameObject germTile = Instantiate(germ[randomNumb], temp, Quaternion.identity, this.transform) as GameObject;
+                iteration = 0;   
+
+                germTile.name = "Germ: (" + i + "; " + j + ")";
+                listOfGerms[i, j] = germTile;
+            }
+        }
+    }
+
+    public bool matchesAt(int col, int row, GameObject germ)
+    {
+        if (col > 1 && row > 1)
+        {
+            if (listOfGerms[col - 1, row].tag == germ.tag && listOfGerms[col - 2, row].tag == germ.tag)
+                return true;
+            if (listOfGerms[col, row - 1].tag == germ.tag && listOfGerms[col, row - 2].tag == germ.tag)
+                return true;
+        }
+        else if (col <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (listOfGerms[col, row - 1].tag == germ.tag && listOfGerms[col, row - 2].tag == germ.tag)
+
+                {
+                    return true;
+                }
+            }
+
+            if (col > 1)
+            {
+                if (listOfGerms[col - 1, row].tag == germ.tag && listOfGerms[col - 2, row].tag == germ.tag)
+                {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public void fillBoardAgain()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if(listOfGerms[i,j] == null)
+
+                {
+                    Vector2 temp = new Vector2(i, j);
+                    int randomNumb = chooseRandomGerm();
+                    
+                            GameObject germTile = Instantiate(germ[randomNumb], temp, Quaternion.identity);                 
+
+                    listOfGerms[i, j] = germTile;
+                    germTile.transform.parent = this.transform;
+                    germTile.name = "Germ: (" + i + "; " + j + ")";
+
+                }
+            }
+        }
+    }
+
+    private bool findCurrentMatchesOnBoard()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                if(listOfGerms[i,j] != null)
+                {
+                    if(listOfGerms[i,j].GetComponent<Germs>().checkMatched)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator fillCooldown()
+    {
+        fillBoardAgain();
+  
+        yield return new WaitForSeconds(.4f);
+        while (findCurrentMatchesOnBoard())
+        {
+            yield return new WaitForSeconds(.4f);
+
+            destroyMatchGerm();
+        }
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (listOfGerms[x, y] != null)
+                {
+                    listOfGerms[x, y].GetComponent<Germs>().lastRow = listOfGerms[x, y].GetComponent<Germs>().germRow;
+                    listOfGerms[x, y].GetComponent<Germs>().lastColumn = listOfGerms[x, y].GetComponent<Germs>().germColumn;
+                    listOfGerms[x, y].transform.parent = this.transform;
+                    listOfGerms[x, y].name = "Germ: (" + x + "; " + y+ ")";
+
+                }
+            }
+        }
+
+    }
+
+    private IEnumerator decreaseRowCooldown()
+    {
+        int count = 0;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++)
+            {
+                if(listOfGerms[i,j] == null)
+                {
+                    count++;
+                }
+                else if (count > 0)
+                {
+                    listOfGerms[i,j].GetComponent<Germs>().germRow -= count;
+                    listOfGerms[i, j] = null;
+                }
+            }
+            count = 0;
+        }
+
+        yield return new WaitForSeconds(.3f);
+        StartCoroutine(fillCooldown());
+    }
+
+
+
+    public void destroyMatchGermAt(int col, int row)
+    {
+        if (listOfGerms[col, row].GetComponent<Germs>().checkMatched)
+        {
+            Destroy(listOfGerms[col, row]);
+            listOfGerms[col, row] = null;
+            Score++;
+            scoreText.text = "Score: " + Score;
+            Debug.Log("You have to destroy " + (maxScore - Score) + " Germ left");
+        }
+    }
+
+    public void destroyMatchGerm()
+    {
+        for(int i = 0; i < width; i++)
+            for(int j = 0; j < height; j++)
+            {
+                if(listOfGerms[i,j] != null)
+                {
+                    destroyMatchGermAt(i, j);
+                   
+                }
+            }
+
+        StartCoroutine(decreaseRowCooldown());       
+    }
+
+    public void setPrePosition()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (listOfGerms[x, y] != null)
+                {
+                    listOfGerms[x, y].GetComponent<Germs>().lastRow = listOfGerms[x, y].GetComponent<Germs>().germRow;
+                    listOfGerms[x, y].GetComponent<Germs>().lastColumn = listOfGerms[x, y].GetComponent<Germs>().germColumn;
+                    listOfGerms[x, y].transform.parent = this.transform;
+                    listOfGerms[x, y].name = "Germ: (" + x + "; " + y + ")";
+
+                }
+            }
+        }
+    }
+
+    public int chooseRandomGerm()
+    {
+        int germChoice = Random.Range(0, 7);
+        return germChoice;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listOfGerms = new GameObject[width, height];
+        createBoard();
+        Debug.Log("You have to destroy " + (maxScore - Score) + " Germ left");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Score >= maxScore)
+        {
+            dimImage.SetActive(true);
+            RetryButton.SetActive(true);
+            Pause = true;
+        }
+
+    }
+}
